4d4613314ad799ff9b56883e399ee51caedb737a
Rodrigo <60452545+rodrigolitzius@users.noreply.github.com>
2026-07-21 18:00:04 -0300

    Fixed /most-played/playlists endpoint
    It actually returned a HashMap and wasnt sorted by played_hours

--- src/backend/src/handlers/playlists.rs
+++ src/backend/src/handlers/playlists.rs
@@ -6,6 +6,7 @@ use crate::{
 
 pub async fn most_played_playlists(
     State(state): State<ApiState>,
+    Query(query): Query<HashMap<String, String>>,
     auth: Auth,
     range: Range<u64>
 ) -> Result<Json<serde_json::Value>, ApiError> {
@@ -21,5 +22,15 @@ pub async fn most_played_playlists(
 
     let playlist_stats = PlaylistStat::group(scrobbles, &playlists, &session.tracks_hashmap);
 
-    return Ok(Json(serde_json::to_value(playlist_stats).unwrap()));
+    let mut response: Vec<PlaylistStat> = playlist_stats.into_iter().map(|p| p.1).collect();
+
+    let mut limit = get_param_default(&query, "limit", response.len());
+    if limit > response.len() {
+        limit = response.len() - 1
+    }
+
+    response.sort_by(|a, b| { b.played_hours.total_cmp(&a.played_hours)});
+    let select = response[..limit].to_vec();
+
+    return Ok(Json(serde_json::to_value(select).unwrap()));
 }
